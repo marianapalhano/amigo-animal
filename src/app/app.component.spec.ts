7d94353572f035d854df39062e0f8cc7b5cdaa4f
Mariana <marianaufc@gmail.com>
2021-12-01 13:53:31 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto-final'`, () => {
+  it(`should have as title 'amigo-animal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-final');
+    expect(app.title).toEqual('amigo-animal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-final app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('amigo-animal app is running!');
   });
 });
